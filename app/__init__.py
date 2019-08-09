6a149c9044f89ef4fca7020698d2304968b0f70c
technoir9 <vimes9@gmail.com>
2019-08-10 00:00:51 +0200

    Try to avoid doubled job() execution

--- app/__init__.py
+++ app/__init__.py
@@ -7,18 +7,19 @@ def job():
     scrape.run()
 
 def create_app(test_config=None):
-    # Configure schedule
-    sched = BackgroundScheduler(daemon=True, timezone="Europe/Warsaw")
-    # sched.add_job(job, 'interval', hours=12, start_date='2019-08-09 22:00:01')
-    sched.add_job(job, 'interval', hours=1, start_date='2019-08-09 18:52:01')
-    sched.start()
-
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+
+    # Configure schedule
+    if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+        sched = BackgroundScheduler(daemon=True, timezone="Europe/Warsaw")
+        # sched.add_job(job, 'interval', hours=12, start_date='2019-08-09 22:00:01')
+        sched.add_job(job, 'interval', hours=12, start_date='2019-08-09 00:02:01')
+        sched.start()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
