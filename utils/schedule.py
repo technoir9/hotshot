c3f58b6ec58a772369fef73782a6e15c775633a0
technoir9 <vimes9@gmail.com>
2019-10-28 23:16:03 +0100

    Update schedule

--- utils/schedule.py
+++ utils/schedule.py
@@ -7,7 +7,7 @@ def job():
 
 def init_scheduler():
     sched = BackgroundScheduler(daemon=True, timezone="Europe/Warsaw")
-    sched.add_job(job, 'interval', hours=12, start_date='2019-08-10 09:00:05')
+    sched.add_job(job, 'interval', hours=12, start_date='2019-08-10 11:00:05')
     sched.start()
     # Shut down the scheduler when exiting the app
     atexit.register(lambda: sched.shutdown())
