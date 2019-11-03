d929610f2a13584ae916eb1b39843584be5a3285
technoir9 <vimes9@gmail.com>
2019-11-03 13:05:16 +0100

    Fix schedule

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,6 @@ def job():
     scrape.run(os.environ['TEMP_WEBHOOK'])
 
 def init_scheduler():
-    sched = BackgroundScheduler(daemon=True, timezone="Europe/Warsaw")
     sched.add_job(job, 'interval', hours=12, start_date='2019-08-10 11:00:05')
     sched.start()
     # Shut down the scheduler when exiting the app
