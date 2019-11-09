93da3ba96d668a632d939c7474613dfffb4148cc
technoir9 <vimes9@gmail.com>
2019-11-09 22:17:28 +0100

    Adjust schedule

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,11 @@ from utils import scrape
 sched = BackgroundScheduler(daemon=True, timezone="Europe/Warsaw")
 
 def job():
+    print('job')
     scrape.run(os.environ['TEMP_WEBHOOK'])
 
 def init_scheduler():
+    print('init_scheduler')
     sched.add_job(job, 'interval', hours=12, start_date='2019-08-10 11:00:05')
     sched.start()
     # Shut down the scheduler when exiting the app
@@ -23,13 +25,16 @@ app.config.from_mapping(
 
 @app.route('/hotshot_run')
 def hotshot_run():
+    print('hotshot_run')
+    # if not app.debug or os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
     if not sched.running:
+        print('sched not running')
         init_scheduler()
     return 'Schedule initialized'
 
-# app.run(use_reloader=False)
 if __name__ == '__main__':
-    app.run()
+    print('__name__ == __main__')
+    app.run(use_reloader=False)
 
 # def create_app(test_config=None):
 #     # create and configure the app
